ebed88e3c79d14d683c40a5e133c3603fe947fd5
Liam Murray <liam@murray.to>
2025-01-28 20:06:32 -0600

    update ask supermemory

--- src/ask-supermemory.tsx
+++ src/ask-supermemory.tsx
@@ -1,17 +1,9 @@
 import { LaunchProps, Detail } from "@raycast/api";
 import { useAskSupermemory } from "../lib/hooks";
 import { showFailureToast } from "@raycast/utils";
-import { useEffect } from "react";
+
 export default function Command(props: LaunchProps<{ arguments: Arguments.AskSupermemory }>) {
   const { answer, isLoading, error } = useAskSupermemory(props.arguments.question);
-
-  useEffect(() => {
-    if (answer) {
-      console.log("answer updated:", answer);
-    }
-  }, [answer]);
-
-  // const dummyAnswer = `#Hello World\n\nThis is a test answer.`;
 
   if (error) {
     console.error(error);
